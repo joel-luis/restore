6ffe58c36f7d8ef1792743935955f9c0f4d6e448
Joel Luis <joelluis73@gmail.com>
2023-09-23 11:16:22 -0300

    install shadcn ui

---
+++ middleware.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next()
+
+  try {
+    const supabase = createMiddlewareClient({
+      req,
+      res,
+    })
+    await supabase.auth.getSession()
+  } catch (error) {
+    console.log('middleware', error)
+  }
+  return res
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1>Restore</h1>
+  return <h1 className="text-lime-700">Restore</h1>
 }
