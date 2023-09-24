baecdd874a20c7727b4b748c6e041deab813c77f
Joel Luis <joelluis73@gmail.com>
2023-09-24 11:08:17 -0300

    Sync Develop (#3)
    * created types for proccess.env
    
    * install shadcn ui
    
    * adjust prettier
    
    * created components and adjust layout home
    
    * finished auth with supabase
    
    * change title metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,24 +6,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { CreateAccountForm } from '@/components/form/createAccountForm'
 import { LoginAccountForm } from '@/components/form/loginAccountForm'
 
-export const dynamic = 'force-static'
-
 export default async function Home() {
-  let loggedIn = false
-
-  try {
-    const supabase = createServerComponentClient({ cookies })
-    const {
-      data: { session },
-    } = await supabase.auth.getSession()
-
-    if (session) loggedIn = true
-  } catch (error) {
-    console.log('Home', error)
-  } finally {
-    if (loggedIn) redirect('/user-app', RedirectType.replace)
-  }
-
   return (
     <div className="flex flex-col h-screen justify-center items-center">
       <Tabs
