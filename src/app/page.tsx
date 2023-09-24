c18b0c6bd3d088e27049806fb0672863c80bc130
Joel Luis <joelluis73@gmail.com>
2023-09-24 13:52:15 -0300

    adjust layout mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import { LoginAccountForm } from '@/components/form/loginAccountForm'
 
 export default async function Home() {
   return (
-    <div className="flex flex-col h-screen justify-center items-center">
+    <div className="flex flex-col h-screen justify-center items-center mx-4">
       <Tabs
         defaultValue="create-account"
-        className="w-[400px] border rounded-md pb-4 shadow-2xl"
+        className="w-full max-w-[400px] border rounded-md pb-4 shadow-2xl"
       >
         <TabsList className="flex justify-around items-center rounded-b-none h-14">
           <TabsTrigger
